481547899a3a9e5409b79dd99c33c070fca209d0
amalms47 <amal@technorip.com>
2018-07-27 10:17:59 +0530

    database new connection 2

--- index.js
+++ index.js
@@ -1,106 +1,40 @@
-const express = require('express');
-const app=express();
-const Joi=require('joi');
+const mongo=require('mongoose');
 
-app.use(express.json());
+//MongoDB connection 
 
+mongo.connect('mongodb://localhost/playground')
+.then(()=>console.log('connected to db'))
+.catch(err=>console.log('could not connect',err));
 
-const courses=[
-	{ id:1,name:'course 1' },
-	{ id:2,name:'course 2' },
-	{ id:3,name:'course 3' },
-]
+//Schema
 
-
-app.get('/', (req, res) => {
-	res.send('New page!!!....');
-});
-
-app.get('/api',(req,res)=>{
-	res.send(courses);
-});
-
-
-// get by id
-
-
-
-app.get('/api/cc/:id',(req,res)=>{
-	const course=courses.find(c=>c.id===parseInt(req.params.id));
-	if(!course) res.status(404).send('Not found');
-	res.send(course);
-})
-
-// insert code
-
-
-app.post('/api/insert',(req,res)=>{
-
-	const {error}=validation(req.body);
-	if(error)return res.status(400).send(error.details[0].message);
-		
-	
-	
-	const course={
-		id:courses.length+1,
-		name:req.body.name			// reading name value from body
-	};
-
-	courses.push(course);
-	res.send(course);
+const courseSchema=new mongo.Schema({
+    name:String,
+    author:String,
+    tags:[String],                              // represents array
+    date:{type:Date,default:Date.now},
+    isPublished:Boolean
 })
 
 
-//updating code
+
+// Creating models
+
+const Course=mongo.model('Course',courseSchema);  //creating a class called Course
 
 
+async function createCourse() {
+    
+ const course=new Course({                            //creating object course  of class Course 
+    name:'Node.js course',
+    author:'Amal',
+    tags:['node','backend'],
+    isPublished:true
+ });
 
-app.put('/api/update/:id',(req,res)=>{
-		
-	const course=courses.find(c=>c.id===parseInt(req.params.id));
-	if(!course) res.status(404).send('Not found');
+ const result=await course.save();
+ console.log(result);
 
-	//validating with other function 
-
-	const { error }=validation(req.body);
-	if(error)return res.status(400).send(error.details[0].message);
-
-	//updating name
-
-	course.name=req.body.name;
-	res.send(course);
-
-})
-
-const port=process.env.port||3000;
-app.listen(3000,() => console.log(`Listening to port ${port}`));	
-
-//delete code
-
-app.delete('/api/delete/:id',(req,res)=>{
-	const course=courses.find(c=>c.id===parseInt(req.params.id));
-	if(!course) res.status(404).send('Not found');
-  
-	const index=courses.indexOf(course);    // finding course index
-	courses.splice(index,1);
-	res.send(course);
-
-});
-
-
-
-// function for validation
-
-
-function validation(val){
-
-	//validation conditions 
-
-	const schema={
-		name:Joi.string().min(3).required()
-	};
-
-	return Joi.validate(val,schema);
-
-	
 }
+
+createCourse();
